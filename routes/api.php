ac19fbcd13f8ab8c78b920a194d3e74d005df19f
Mateusz Zolisz <mateusz.zolisz@gmail.com>
2021-12-13 21:18:18 +0100

    implement ArticleController

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,11 @@
 <?php
 
-use App\Models\Article;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('articles', function() {
-    return Article::all();
-});
-
-Route::get('articles/{id}', function($id) {
-    return Article::find($id);
-});
-
-Route::post('articles', function(Request $request) {
-    return Article::create($request->all());
-});
-
-Route::put('articles/{id}', function(Request $request, $id) {
-    $article = Article::FindOrFail();
-    $article->update($request->all());
-
-    return $article;
-});
-
-Route::delete('articles/{id}', function($id) {
-    Article::find($id)->delete();
-
-    return 204;
-});
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{article}', [ArticleController::class, 'show']);
+Route::post('/articles', [ArticleController::class, 'store']);
+Route::put('/articles/{article}', [ArticleController::class, 'update']);
+Route::delete('/article/{article}', [ArticleController::class, 'delete']);
